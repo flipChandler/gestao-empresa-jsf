ea277a741a8a94733cfdc86f55188ff9789601b6
felipesantos11 <felipe.santos11@hotmail.com>
2024-01-08 10:50:18 -0300

    feat: search flow and faces messages

--- gestao-empresa-jsf/src/main/java/br/com/felipesantos/erp/controller/GestaoEmpresasBean.java
+++ gestao-empresa-jsf/src/main/java/br/com/felipesantos/erp/controller/GestaoEmpresasBean.java
@@ -10,6 +10,7 @@ import javax.inject.Named;
 
 import br.com.felipesantos.erp.model.Empresa;
 import br.com.felipesantos.erp.repository.EmpresaRepository;
+import br.com.felipesantos.erp.util.FacesMessages;
 
 @Named
 @ViewScoped
@@ -20,20 +21,39 @@ public class GestaoEmpresasBean implements Serializable {
 	@Inject
 	private EmpresaRepository empresaRepository;
 		
+	@Inject
+	private FacesMessages facesMessages;
+	
 	private List<Empresa> empresas = new ArrayList<>();
+	
+	private String termoPesquisa;
 	
 	public void buscarTodas() {
 		this.empresas = empresaRepository.findAll();
 	}
 	
+	public void pesquisar() {
+		empresas = empresaRepository.buscarPorNomeFantasia(termoPesquisa);
+		if (empresas.isEmpty()) {
+			facesMessages.info("Sua consulta não retornou registros!");
+		}
+	}
+	
 	public List<Empresa> getEmpresas() {
 		return empresas;
 	}
-	
+
+	public String getTermoPesquisa() {
+		return termoPesquisa;
+	}
+
+	public void setTermoPesquisa(String termoPesquisa) {
+		this.termoPesquisa = termoPesquisa;
+	}	
 	// opções pra buscar todas as empresas quando iniciar a aplicação
 	//  <f:event listener="#{gestaoEmpresasBean.buscarTodas}" type="preRenderView" /> -- dentro de <h:body>
 	// ou
 	//  <f:metadata>
 	//         <f:viewAction action="#{gestaoEmpresasBean.buscarTodas}" /> -- dentro do <h:head>
-	//  </f:metadata>
+	//  </f:metadata>	
 }
